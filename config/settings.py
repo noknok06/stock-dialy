10bec9497189ac21d646ac33705cce3d6e179aa8
noknok06 <nok.h0618@gmail.com>
2025-03-09 23:00:59 +0900

    ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h=(#w%%7ejhe=u$vjre&d%6u(-7a$js2x4*v76iq4_m3+7onk#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['hondy.pythonanywhere.com', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://hondy.pythonanywhere.com', 'http://hondy.pythonanywhere.com']
 
 
