cc081b0346e3fc25e64b43694d253272592a2497
noknok06 <nok.h0618@gmail.com>
2025-03-13 22:35:09 +0900

    Revert "update"
    This reverts commit cdaa0e3207ddaeb568cac0c53d5bc6d0f2a8219a.

--- config/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect
-
-def landing_page(request):
-    # ユーザーがログイン済みの場合は直接アプリのホームページへリダイレクト
-    if request.user.is_authenticated:
-        return redirect('stockdiary:home')
-    
-    # ランディングページを表示
-    return render(request, 'landing_page.html')
--- users/views.py
+++ users/views.py
@@ -73,11 +73,3 @@ class AccountDeleteView(LoginRequiredMixin, DeleteView):
         user.delete()
         
         return redirect(self.success_url)
-
-def landing_page(request):
-    # ユーザーがログイン済みの場合は直接アプリのホームページへリダイレクト
-    if request.user.is_authenticated:
-        return redirect('stockdiary:home')
-    
-    # ランディングページを表示
-    return render(request, 'landing_page.html')
