41d31a6096bafb9016a941c4768458af615eb5dd
noknok06 <nok.h0618@gmail.com>
2025-04-09 23:16:23 +0900

    自動テスト修正

--- config/test_urls.py
+++ config/test_urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('users/', include('users.urls')),  # usersアプリのURLを追加
     path('', include('stockdiary.urls')),
     path('checklist/', include('checklist.urls')),
     path('tags/', include('tags.urls')),
