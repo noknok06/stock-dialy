ee5b01971d201b686d0e04bf2768bb6420a5bd3f
naoki <nok.h0618@gmail.com>
2026-03-09 01:07:33 +0900

    fix

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,5 @@
 // static/sw.js - シンプル版
-const VERSION = '1.0.18';  // ← CSS変更時はここだけ変更すればOK
+const VERSION = '1.0.20';  // ← CSS変更時はここだけ変更すればOK
 const CACHE_NAME = `kabulog-v${VERSION}`;
 const STATIC_CACHE_NAME = `kabulog-static-v${VERSION}`;
 
